ed9a1abd1f4604a2ac3bb28a693e1f2e5680305f
yncynotdev <yncyncy0@gmail.com>
2026-04-28 17:37:20 +0000

    update: used vim.pack as package manager (for experimental purposes) 	- put lazy.vim package manager config -> /nvim.bak

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,12 +1,21 @@
--- Retrieves configuration at "/nvim/lua/config" directory
--- @param {string} get the path directory via config.*
---
--- @example
--- if you have "nvim/lua/config/keymaps"
--- require("config.keymaps") -- where the directory is in "/config/keymaps"
+vim.pack.add({
+	'https://github.com/rose-pine/neovim',
+	'https://github.com/nvim-treesitter/nvim-treesitter',
+	'https://github.com/nvim-telescope/telescope.nvim',
+	'https://github.com/nvim-telescope/telescope-ui-select.nvim',
+	'https://github.com/nvim-lua/plenary.nvim',
+	'https://github.com/stevearc/oil.nvim',
+	'https://github.com/nvim-mini/mini.icons',
+	'https://github.com/nvim-mini/mini.pairs',
+	'https://github.com/nvim-mini/mini.comment',
+	'https://github.com/windwp/nvim-ts-autotag',
+})
 
-require("config.lazy")
-require("config.keymaps")
+
+-- Default Keymaps
+vim.g.mapleader = " "
+vim.keymap.set("i","jj", "<Esc>", {noremap = true, silent = true})
+
 
 -- Custom Vim configuration
 vim.cmd("set expandtab")
@@ -26,7 +35,75 @@ vim.api.nvim_create_autocmd("ColorScheme", {
   end,
 })
 
--- local projectfile = vim.fn.getcwd() .. '/project.godot'
--- if projectfile then
---     vim.fn.serverstart './godothost'
--- end
+
+-- Color Scheme
+vim.cmd.colorscheme('rose-pine')
+
+
+-- oil.nvim config
+require('oil').setup()
+vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+
+
+-- telescope config
+local builtin = require("telescope.builtin")
+require("telescope").setup({
+	extensions = {
+	  ["ui-select"] = {
+	    require("telescope.themes").get_dropdown({}),
+	  },
+	},
+	defaults = {
+	  cwd = vim.fn.getcwd(),
+	  file_ignore_patterns = {
+	    "node_modules",
+	    ".git/",
+	    ".nuxt",
+	  },
+	},
+	pickers = {
+	  find_files = {
+	    hidden = true,
+	  },
+	}
+})
+require("telescope").load_extension("ui-select")
+vim.keymap.set("n", "<leader>p", builtin.find_files, { desc = "Telescope find files" })
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+
+
+-- treesitter
+require("nvim-treesitter").setup({
+	build = ':TSUpdate',
+	auto_install = true,
+	highlight = { enable = true },
+	indent = { enable = false }
+})
+
+
+-- mini.nvim(the goat)
+require("mini.pairs").setup()
+require("mini.comment").setup()
+
+require("mini.icons").setup({
+	style = "glyph",
+})
+
+
+-- auto tags
+require('nvim-ts-autotag').setup({
+	opts = {
+	  -- Defaults
+	  enable_close = true,    -- Auto close tags
+	  enable_rename = true,   -- Auto rename pairs of tags
+	  enable_close_on_slash = false -- Auto close on trailing </
+	},
+	-- Also override individual filetype configs, these take priority.
+	-- Empty by default, useful if one of the "opts" global settings
+	-- doesn't work well in a specific filetype
+	per_filetype = {
+	  ["html"] = {
+	    enable_close = false
+	  }
+	}
+})
